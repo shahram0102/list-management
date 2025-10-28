2b96f14c1f65b6b994b685ab7e3aa776bb6181b0
shahram0102 <shahramkhaleh2001@gmail.com>
2025-10-28 22:42:06 +0330

    refactor(ui): improve layout and add state management to Home component
    Enhanced the main application layout by adjusting the header to maintain a consistent width across different screen sizes and ensuring responsiveness. In the Home component, introduced state management using useState for future item handling capabilities. Added a basic layout structure with placeholder content and a "Create" button for potential list management functionality. These changes set the groundwork for further development of item management features.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="px-3 py-2 md:px-6 md:py-4 flex items-center justify-end">
-        <ModeToggle />
+    <div className="min-h-screen flex flex-col w-full">
+      <header className="max-w-7xl mx-auto w-full">
+        <div className="px-4 py-3 md:px-6 md:py-4 xl:px-8 xl:py-6 flex items-center justify-end">
+          <ModeToggle />
+        </div>
       </header>
       <Home />
     </div>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [items, setItems] = useState<Item[]>([]);
+
+  return (
+    <main className="h-full flex-1 flex px-4 py-3 md:px-6 md:py-4 xl:px-8 xl:py-6 flex-col justify-between max-w-7xl mx-auto w-full">
+      <div className="flex-1 flex flex-col gap-2 md:gap-4 xl:gap-6">
+        <h1 className="text-3xl md:text-4xl font-bold">List Management</h1>
+        <p className="text-base md:text-lg text-muted-foreground">
+          Manage your items easily — view, add, edit, or delete them.
+        </p>
+      </div>
+      <div className="w-full flex items-center justify-end">
+        <Button>Create</Button>
+      </div>
+    </main>
+  );
 }
