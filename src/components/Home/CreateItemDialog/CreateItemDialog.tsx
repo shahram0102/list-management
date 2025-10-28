f543ea3021914d01902f0640957d2a4405a48195
shahram0102 <shahramkhaleh2001@gmail.com>
2025-10-29 00:42:20 +0330

    feat(create-item-dialog): add component for creating new items
    Introduce a new CreateItemDialog component that allows users to create and add new items via a form. The form uses react-hook-form with zod for validation, ensuring the title and subtitle meet specified constraints. On successful submission, an item is added with a unique ID, and a success toast is displayed. This addition enhances the user interface by providing a dialog-driven creation flow.

---
+++ src/components/Home/CreateItemDialog/CreateItemDialog.tsx
@@ -0,0 +1,96 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+type CreateItemDialogProps = {
+  onAdd: (item: Item) => void;
+};
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  subTitle: z.string().min(10).max(200),
+});
+
+export function CreateItemDialog({ onAdd }: CreateItemDialogProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const id = Date.now().toString();
+    onAdd({ ...values, id });
+    toast.success("New item add successfully.");
+    form.reset({
+      title: "",
+      subTitle: "",
+    });
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader className="text-start">
+          <DialogTitle>Create Item</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="subTitle"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Subtitle</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Subtitle" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end">
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
